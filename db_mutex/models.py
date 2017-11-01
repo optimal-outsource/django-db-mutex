310204850814f886edfee2e1c62d59b438ffe080
Adam Stein <adam@csh.rit.edu>
2017-11-01 11:07:27 -0400

    Fixed CharField length issue

--- db_mutex/models.py
+++ db_mutex/models.py
@@ -5,5 +5,5 @@ class DBMutex(models.Model):
     """
     Models a mutex lock with a lock ID and a creation time.
     """
-    lock_id = models.CharField(max_length=256, unique=True)
+    lock_id = models.CharField(max_length=255, unique=True)
     creation_time = models.DateTimeField(auto_now_add=True)
